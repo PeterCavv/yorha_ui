955e647f7bb7a5a497299503c514755ae54a3277
PeterCavv <pedroabad52@gmail.com>
2024-02-29 08:34:53 +0100

    Cambios para hacer Infos responsivos

--- src/components/AndroidPage.vue
+++ src/components/AndroidPage.vue
@@ -1,39 +1,37 @@
 <template>
     <h2>{{ $t('data_base.android_data') }}</h2>
-    <table>
-        <tr>
-            <td style="padding-right:10rem; ">
-                <input v-model="searchValue" type="text" v-bind:placeholder="$t('data_search.android_search')"
-                style="width:20rem; margin-left: 3rem;">
+    <input v-model="searchValue" type="text" v-bind:placeholder="$t('data_search.android_search')"
+    style="width:20rem; margin-left: 3rem;">
 
-                <blockquote class="full">
-                    <figure class="data-figure">
-                        <ul>
-                            <div v-if="androidList.length">
-                                <div v-for="(android, index) in androidList" :key="index">
-                                    <li style= "margin-top: 5px;">
-                                        <button @click="showTypeInfo(android)" 
-                                        class="button button-list" id="menu" style="margin-bottom: 10px;">
-                                            {{ android.name }}
-                                            <img v-if="android.state.name === 'Operational'" 
-                                            src="../assets/Operative_Icon.png" width="12" height="10" 
-                                            style="vertical-align: middle; float:right"/>
-                                            <img v-else src="../assets/No_Operative_Icon.png" width="10" height="8" 
-                                            style="vertical-align: middle; float:right"/>
-                                        </button>
-                                    </li>
-                                </div>
-                            </div>
-                            <div v-else style="text-align: left;">{{ $t('data_search.reports_message')}}</div>
-                        </ul>
-                    </figure>
-                </blockquote>
-            </td>
+    <div class="flex-container">
+        <blockquote style="width: 25rem; margin-right: 6rem;">
+            <figure class="data-figure" style="height: 100%;">
+                <hr/>
+                <ul>
+                    <div v-if="androidList.length">
+                        <div v-for="(android, index) in androidList" :key="index">
+                            <li style= "margin-top: 5px;">
+                                <button @click="showTypeInfo(android)" 
+                                class="button button-list" id="menu" style="margin-bottom: 10px; width: 100%;">
+                                    {{ android.name }}
+                                    <img v-if="android.state.name === 'Operational'" 
+                                    src="../assets/Operative_Icon.png" width="12" height="10" 
+                                    style="vertical-align: middle; float:right"/>
+                                    <img v-else src="../assets/No_Operative_Icon.png" width="10" height="8" 
+                                    style="vertical-align: middle; float:right"/>
+                                </button>
+                            </li>
+                        </div>
+                    </div>
+                    <div v-else style="text-align: left;">{{ $t('data_search.reports_message')}}</div>
+                </ul>
+                <hr/>
+            </figure>
+        </blockquote>
 
-            <InfoWindow :dataUse="selectedAndroid" :dataType="'android'" :addWindow="addWindow"/>
+        <InfoWindow :dataUse="selectedAndroid" :dataType="'android'" :addWindow="addWindow"/>
+    </div>
 
-        </tr>
-    </table>
     <hr/>
 </template>
 
