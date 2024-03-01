a6f897e75ff6287cbb77990ca580687059212d7c
PeterCavv <pedroabad52@gmail.com>
2024-03-01 12:22:19 +0100

    Cambios para adaptar info androides

--- src/components/AndroidPage.vue
+++ src/components/AndroidPage.vue
@@ -4,32 +4,34 @@
     style="width:20rem; margin-left: 3rem;">
 
     <div class="flex-container">
-        <blockquote style="width: 25rem; margin-right: 6rem;">
+        <blockquote style="width: 25rem; margin-right: 8rem;">
             <figure class="data-figure" style="height: 100%;">
                 <hr/>
-                <ul>
                     <div v-if="androidList.length">
-                        <div v-for="(android, index) in androidList" :key="index">
-                            <li style= "margin-top: 5px;">
-                                <button @click="showTypeInfo(android)" 
-                                class="button button-list" id="menu" style="margin-bottom: 10px; width: 100%;">
+                        <div v-for="( android, index ) in androidList" :key="index">
+                                <button @click="showTypeInfo( android )" 
+                                class="button button-list" id="menu" style="margin-bottom: 6px; width: 100%;">
+                                    <img v-if="android.model.name == 'YoRHa'" src="../assets/android_icon.png" 
+                                    class="img-android"/>
+                                    <img v-else src="../assets/special_icon.png" 
+                                    class="img-nav"/>
+
                                     {{ android.name }}
+
                                     <img v-if="android.state.name === 'Operational'" 
                                     src="../assets/Operative_Icon.png" width="12" height="10" 
                                     style="vertical-align: middle; float:right"/>
                                     <img v-else src="../assets/No_Operative_Icon.png" width="10" height="8" 
                                     style="vertical-align: middle; float:right"/>
                                 </button>
-                            </li>
                         </div>
                     </div>
                     <div v-else style="text-align: left;">{{ $t('data_search.reports_message')}}</div>
-                </ul>
                 <hr/>
             </figure>
         </blockquote>
 
-        <InfoWindow :dataUse="selectedAndroid" :dataType="'android'" :addWindow="addWindow"/>
+        <InfoWindow :dataUse="selectedAndroid" :dataType="'android'" :addWindow="addWindow" style="margin-top: -35px;"/>
     </div>
 
     <hr/>
@@ -64,3 +66,11 @@ export default {
     }
 }
 </script>
+
+<style>
+.img-android{
+    width: 20px;
+    height: 23px;
+    vertical-align: middle;
+}
+</style>
