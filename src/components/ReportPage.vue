d23a67b60bf24dc8f8fbecfc2ae9ec5aaa5dbc53
PeterCavv <pedroabad52@gmail.com>
2024-10-17 20:29:58 +0200

    Revert "Comentado estilo modal"
    This reverts commit 6f5ff1ac47ddf92b6319bf47e3f64f8d9d541e83.

--- src/components/ReportPage.vue
+++ src/components/ReportPage.vue
@@ -22,7 +22,7 @@
             </figure>
         </blockquote>
 
-        <InfoWindow :dataUse="selectedReport" :dataType="'report'" :addWindow="addWindow" style="margin-top: -35px;"/>
+        <InfoWindow :dataUse="selectedReport" :dataType="'report'" :addWindow="addWindow" />
     </div>
     <hr/>
 </template>
