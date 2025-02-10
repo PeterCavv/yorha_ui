2fe34e544e8d3362c28a37398806295f54d857d3
PeterCavv <pedroabad52@gmail.com>
2025-02-10 19:45:56 +0100

    Modificado infoWindow para armas

--- src/components/ArmoryPage.vue
+++ src/components/ArmoryPage.vue
@@ -1,3 +1,9 @@
+<script setup>
+import { ref } from 'vue';
+
+const addWindow = ref(false);
+</script>
+
 <template>
     <h2>{{ $t('data_base.armory_data') }}</h2>
     <input v-model="searchValue" type="text" v-bind:placeholder="$t('data_search.weapons_search')"
@@ -9,7 +15,7 @@
                 <hr/>
                 <div v-if="weaponList.length" class="dataScroll">
                     <div v-for="( weapon, index ) in weaponList" :key="index">
-                        <button @click="showTypeInfo( weapon )" 
+                        <button @click="() => {showTypeInfo( weapon ); addWindow = true}" 
                         class="button button-list" id="menu" style="width: 100%">
 
                             {{ weapon.name }}
@@ -22,7 +28,38 @@
             </figure>
         </blockquote>
 
-        <InfoWindow class="infoWindow" :dataUse="selectedWeapon" :dataType="'weapon'" :addWindow="addWindow" style="margin-top: -35px;"/>
+        <InfoWindow class="infoWindow" :addWindow="addWindow" style="margin-top: -35px;">
+            <template #title>
+                {{ $t("weapon.info") }}
+            </template>
+            <template #body>
+                <div v-if="selectedWeapon.weapon_type.name !='COMBAT_BRACERS'" class="imageBox">
+                    <div v-if="selectedWeapon.weapon_type.name == 'SMALL_SWORDS'">
+                        <img v-if="selectedWeapon.name == 'YoRHa-issue Blade'" class="weaponimg" src="../assets/yorha-issue_blade_img.png">
+                        <img v-else-if="selectedWeapon.name == 'Cruel Oath'" class="weaponimg" src="../assets/cruel_oath_img.png">
+                        <img v-else class="weaponimg" src="../assets/virtuous_contract_img.png">
+                    </div>
+                    <div v-else-if="selectedWeapon.weapon_type.name == 'LARGE_SWORDS'">
+                        <img v-if="selectedWeapon.name == 'Virtuous Treaty'" class="weaponimg" src="../assets/virtuous_treaty_img.png">
+                        <img v-else class="weaponimg" src="../assets/beastlord_img.png">
+                    </div>
+                    <div v-else>
+                        <img class="weaponimg" src="../assets/virtuous_dignity_img.png">
+                    </div>
+                </div>
+                <div v-else class="imageBoxBracers">
+                    <img class="weaponbarefistimg" src="../assets/barefist_img.png">
+                </div>
+                <div class="inOneLine">
+                    <cite>{{ selectedWeapon.name }}</cite>
+                    <cite style="margin-left: auto;">{{ changeString(selectedWeapon.weapon_type.name) }}</cite>
+                </div>
+                <hr/>
+                <p v-if="selectedWeapon.desc.length != 0">{{ selectedWeapon.desc }}</p>
+                <p v-else style="text-transform: uppercase;">{{ $t('information.null_desc') }}</p>
+                <p></p>
+            </template>
+        </InfoWindow>
     </div>
     <hr/>
 </template>
@@ -45,14 +82,18 @@ import InfoWindow from './InfoWindow.vue';
         },
         data: {
             selectedWeapon: '',
-            addWindow: 0
         },
         mixins: [searcher],
         methods: {
             showTypeInfo(weapon){
                 this.selectedWeapon = weapon;
-                this.addWindow = 1;
-            }
+            },
+            changeString(data){
+            return data.split('_').map(word =>
+                word.charAt(0).toUpperCase() + 
+                word.slice(1).toLowerCase()
+            ).join(' ');
+        }
         }
 }
 
--- src/components/InfoWindow.vue
+++ src/components/InfoWindow.vue
@@ -15,56 +15,6 @@
             </figcaption>
             <slot name="body"></slot>
         </figure>
-
-        <!--REPORT INFO -->
-
-        <!-- END REPORT INFO -->
-
-        <!-- OPERATOR INFO -->
-
-        <!-- END OPERATOR INFO -->
-
-        <!-- TYPES INFO -->
-
-        <!-- END TYPE INFO -->
-
-        <!-- WEAPONS INFO -->
-        <div v-if="dataType == 'weapon'">
-            <figure class="innerbox">
-                <figcaption style="text-transform: uppercase;">
-                    <img src="../assets/Info_Icon.png" width="22" height="19"
-                    style="vertical-align: middle;"/>
-                    {{ $t("weapon.info") }}
-                </figcaption>
-                <div v-if="dataUse.weapon_type.name !='COMBAT_BRACERS'" class="imageBox">
-                    <div v-if="dataUse.weapon_type.name == 'SMALL_SWORDS'">
-                        <img v-if="dataUse.name == 'YoRHa-issue Blade'" class="weaponimg" src="../assets/yorha-issue_blade_img.png">
-                        <img v-else-if="dataUse.name == 'Cruel Oath'" class="weaponimg" src="../assets/cruel_oath_img.png">
-                        <img v-else class="weaponimg" src="../assets/virtuous_contract_img.png">
-                    </div>
-                    <div v-else-if="dataUse.weapon_type.name == 'LARGE_SWORDS'">
-                        <img v-if="dataUse.name == 'Virtuous Treaty'" class="weaponimg" src="../assets/virtuous_treaty_img.png">
-                        <img v-else class="weaponimg" src="../assets/beastlord_img.png">
-                    </div>
-                    <div v-else>
-                        <img class="weaponimg" src="../assets/virtuous_dignity_img.png">
-                    </div>
-                </div>
-                <div v-else class="imageBoxBracers">
-                    <img class="weaponbarefistimg" src="../assets/barefist_img.png">
-                </div>
-                <div class="inOneLine">
-                    <cite>{{ dataUse.name }}</cite>
-                    <cite style="margin-left: auto;">{{ changeString(dataUse.weapon_type.name) }}</cite>
-                </div>
-                <hr/>
-                <p v-if="dataUse.desc.length != 0">{{ dataUse.desc }}</p>
-                <p v-else style="text-transform: uppercase;">{{ $t('information.null_desc') }}</p>
-                <p></p>
-
-            </figure>
-        </div>
-
     </div>
 </template>
 
@@ -73,50 +23,10 @@
 export default {
     name: "InfoWindow",
     props: {
-        dataUse: {
-            type: Object,
-            required: false
-        },
-        dataType: {
-            type: String,
-            required: true
-        },
         addWindow: {
             type: Boolean,
             required: false
         }
-    },
-    data: function() {
-        return{
-            editVal: "",   
-        }
-    }, 
-    methods: {
-        changeString(data){
-            return data.split('_').map(word =>
-                word.charAt(0).toUpperCase() + 
-                word.slice(1).toLowerCase()
-            ).join(' ');
-        },
-        compareDates(date){
-            if(date === null){
-                return false;
-            }
-
-            const formatDateToYYYYMMDD = () => {
-                const [day, month, year] = this.dataUse.publish_date.split('/');
-                return `${year}-${month}-${day}`;
-            }
-
-            date = formatDateToYYYYMMDD();
-
-            const inputDate = new Date(date);
-            const today = new Date();
-            today.setHours(0, 0, 0, 0);
-            inputDate.setHours(0, 0, 0, 0);
-
-            return inputDate.getTime() > today.getTime();
-        },
     }
 }
 </script>
