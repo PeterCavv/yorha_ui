581dc047d47a004c8e30d7d8e610350ebf973061
PeterCavv <pedroabad52@gmail.com>
2025-02-11 19:11:03 +0100

    Autenticación modificada

--- src/stores/UserStore.js
+++ src/stores/UserStore.js
@@ -6,12 +6,17 @@ export const useAuthStore = defineStore('auth', {
     state: () => ({
         user: {
             name: null,
-            role: 'admin'
+            role: null
         }
     }),
     getters: {
         hasRole: (state) => !!state.user.role,
         isAdmin: (state) => state.user.role === 'admin',
         isUser: (state) => state.user.role === 'user'
+    },
+    actions: {
+        setRole(role){
+            this.user.role = role;
+        }
     }
 });
