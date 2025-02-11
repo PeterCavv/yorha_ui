a251e063fd9073c2c09bd976596e29ef91fa5879
PeterCavv <pedroabad52@gmail.com>
2025-02-11 17:26:03 +0100

    Modificado store para usuarios

--- src/stores/UserStore.js
+++ src/stores/UserStore.js
@@ -1,2 +1,17 @@
 import { defineStore } from "pinia";
 
+//Temporal values
+
+export const useAuthStore = defineStore('auth', {
+    state: () => ({
+        user: {
+            name: null,
+            role: 'user'
+        }
+    }),
+    getters: {
+        hasRole: (state) => !!state.user.role,
+        isAdmin: (state) => state.user.role === 'admin',
+        isUser: (state) => state.user.role === 'user'
+    }
+});
